7721331c5d98a9f9bb9f74710e33618490fc3cd5
DanielGreens <stolittsy@mail.ru>
2019-11-28 10:55:10 +0300

    Initial commit for pod

---
+++ FlawlessValidation/FlawlessValidation.h
@@ -0,0 +1,19 @@
+//
+//  FlawlessValidation.h
+//  FlawlessValidation
+//
+//  Created by OMELCHUK Daniil on 28/11/2019.
+//  Copyright © 2019 OMELCHUK Daniil. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FlawlessValidation.
+FOUNDATION_EXPORT double FlawlessValidationVersionNumber;
+
+//! Project version string for FlawlessValidation.
+FOUNDATION_EXPORT const unsigned char FlawlessValidationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FlawlessValidation/PublicHeader.h>
+
+
---
+++ FlawlessValidationExamples/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FlawlessValidationExamples
+//
+//  Created by OMELCHUK Daniil on 28/11/2019.
+//  Copyright © 2019 OMELCHUK Daniil. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
